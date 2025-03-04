57107a9c44301bdb6afb9c26cec9faa3ffdeee20
Angshuman Kalita <117644075+AngshumanKalita@users.noreply.github.com>
2025-03-04 21:59:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Toggle the mobile navigation menu
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('mobile-menu');
+    const navMenu = document.querySelector('.nav-menu');
+  
+    menuToggle.addEventListener('click', () => {
+      navMenu.classList.toggle('active');
+    });
+  });
+  
